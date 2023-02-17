ffcac4646eb278516ba201ebdd2454d4d7f9b7c8
Mozakir178 <mozakir178@gmail.com>
2023-02-17 22:13:30 +0530

    Testing Assignment done

---
+++ src/main/java/Runner.java
@@ -0,0 +1,10 @@
+import dao.PersonDao;
+import dao.PersonDaoImpl;
+import entities.Person;
+import exceptions.PersonException;
+
+public class Runner {
+    public static void main(String[] args) throws PersonException {
+//      use this method to test your application
+    }
+}
